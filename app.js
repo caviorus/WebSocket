dadc1bf87172762ecd35dac90bc073fe292facc3
Caviorus <xlucief@gmail.com>
2023-08-21 21:10:28 +0700

    simple peer to peer connection

--- app.js
+++ app.js
@@ -6,35 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-//mysql
-// const mysql = require('mysql');
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'ws',
-// });
-
-// db.connect();
-
 const clients = [];
 
 wss.on('connection', (ws, request) => {
   // generate id
   const id = Math.random().toString(36).slice(2);
   console.log(`Client connected with id : ${id}`);
-  // const url = new URL(request.url, 'ws://localhost:3000');
-  // const clientId = url.searchParams.get('client-id');
-  // const appId = url.searchParams.get('app-id');
-  // const appChannel = url.searchParams.get('app-channel'); 
-
-  // db.query(`SELECT * FROM users where access_key = '${clientId}'`, function(error, result, fields){
-  //   console.log(error);
-  //   console.log(result.length);
-  // });
-
-  // console.log(`connected with parameters : ${clientId},${appId},${appChannel}`);
-
+  
   // store id
   ws.id = id;
   clients[id] = ws;
@@ -42,11 +20,10 @@ wss.on('connection', (ws, request) => {
   // message handler
   ws.on('message', (message) => {
     message = JSON.parse(message);
-    console.log(`Received message: ${message.text}`);
 
     let messageSent = false;
     wss.clients.forEach((client) => {
-      if (typeof message.to != 'undefined') {
+      if (typeof message.to == 'undefined') {
         // broadcast message
         if (client.readyState === WebSocket.OPEN) {
           client.send(message.text);
@@ -54,11 +31,8 @@ wss.on('connection', (ws, request) => {
       }
       else {
         // privat message
-        const recipientId = message.to;
-        const recipient = clients[recipientId];
-        if (recipient && !messageSent) {
-          messageSent = true;
-          recipient.send(`From ${id}: ${message.text}`);
+        if(client == clients[message.to]){
+          client.send(message.text);
         }
       }
     });
